7f9a73605e4710e4b56552001c2800522db1ca57
JarupongMin <129025834+mdeenit@users.noreply.github.com>
2024-08-27 20:35:56 +1000

    dsd

--- scripts/src/dataLists.js
+++ scripts/src/dataLists.js
@@ -12,18 +12,49 @@ let dataArray = [
 	"toyota corolla"
 ];
 
-
+// Find elements on the page for us to use the logic 
 let carsContainerElement = document.getElementById("carsContainer");
+
+// Create elements to add to the page via the logic 
 let carsContainerList = document.createElement("ul");
 
-
+function renderData() {
+// Do logic for each individual item in the array 
 dataArray.forEach((car) => {
+
+	// Verify what we are working with, just log it to see the data's value 
 	console.log(car);
 
+	// Create a new HTML element to help us format the data's value 
 	let newCarEntry = document.createElement("li");
 
+	// Add the data to the new element 
+	newCarEntry.innerText += car;
+
+	// Add a button to each entry 
+	// click on the button to reomove the entry from the list 
+	let removeButton = document.createElement("button");
+	// removeButton.innerButton to the car entry 
+	removeButton.innerText = 'Remove ${car}'; 
+	// Add the nicely-formatted element into the list container 
 	carsContainerElement.appendChild(newCarEntry);
+	removeButton. 
+	removeButton.onclick = (() => removeCarFromDataList(car));
 });
+	
+	carsContainerElement.appendChild(carsContainerElement);
 
 
 
+}
+
+
+
+
+
+
+function removeCarFromDataList (targetItemToRemove) {
+	dataArray. data.Array.filter((car) => car != targetItemToRemove);
+
+	renderData();
+}
